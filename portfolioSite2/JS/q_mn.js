e975f0dca6a5781c68a521384d03b591464ad53a
sangyong park <76932869+sangyoung23@users.noreply.github.com>
2022-06-24 17:04:16 +0900

    Add files via upload

--- portfolioSite2/JS/q_mn.js
+++ portfolioSite2/JS/q_mn.js
@@ -5,3 +5,11 @@ $(document).ready(function(){
       $("#q_mn").stop().animate({"top":position+currentPosition+"px"},800);
     });
   });
+
+  $(document).ready(function(){
+    var currentPosition = parseInt($("#q_icon").css("top"));
+    $(window).scroll(function() {
+      var position = $(window).scrollTop(); 
+      $("#q_icon").stop().animate({"top":position+currentPosition+"px"},800);
+    });
+  });
