3021108092db432cfa1d6f619282d344b5520c02
sangyong park <76932869+sangyoung23@users.noreply.github.com>
2022-07-29 10:17:20 +0900

    Delete portfolioSite2 directory

--- portfolioSite2/JS/index.js
+++
@@ -1,8 +0,0 @@
-const moveLi = document.querySelectorAll("#head nav ul li");
-const aboutMe = document.getElementById("#aboutme").offsetTop;
-// const firstTop = aboutMe[1].offsetTop;
-
-
-moveLi[0].onclick = () => {
-  window.scroll({top:aboutMe, behavior: "smooth"})
-}
